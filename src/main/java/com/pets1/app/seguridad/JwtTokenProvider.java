ba53da9a09410e13acd58b09b4968ae73cca884e
juan david marin <juaanmarin2007@gmail.com>
2022-08-06 11:09:29 -0500

    se agrego los datos adicionales para generar el token

--- src/main/java/com/pets1/app/seguridad/JwtTokenProvider.java
+++ src/main/java/com/pets1/app/seguridad/JwtTokenProvider.java
@@ -1,13 +1,23 @@
 package com.pets1.app.seguridad;
 
+import java.util.Collection;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import com.pets1.app.domain.ClinicaVo;
+import com.pets1.app.domain.UsuarioVo;
+import com.pets1.app.domain.VeterinarioVo;
 import com.pets1.app.exeptions.AppPetsCareExeption;
+import com.pets1.app.repository.IClinicaRepository;
+import com.pets1.app.repository.IUsuarioRepository;
+import com.pets1.app.repository.IVeterinarioRepository;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
@@ -21,6 +31,16 @@ import io.jsonwebtoken.UnsupportedJwtException;
 @Component
 public class JwtTokenProvider {
 	
+	@Autowired
+	private IUsuarioRepository usuarioRepository;
+	
+	@Autowired 
+	private IClinicaRepository clinicaRepository;
+	
+	@Autowired
+	private IVeterinarioRepository veterinarioRepository;
+	
+	
 	@Value("${app.jwt-secret}")
 	private String jwtSecret;
 	
@@ -29,10 +49,16 @@ public class JwtTokenProvider {
 	
 	public String generarToken(Authentication authentication) {
 		String userName = authentication.getName();
+		
+		Collection<? extends GrantedAuthority> rolUsu = authentication.getAuthorities();
+		String rol = rolUsu.toString();
+		
+		String usuario = idDeUsuario(userName);
+		
 		Date fechaActual = new Date();
 		Date fechaExpiracion = new Date(fechaActual.getTime() + jwtExpirationInMs);
 		
-		String token = Jwts.builder().setSubject(userName).setIssuedAt(new Date()).setExpiration(fechaExpiracion)
+		String token = Jwts.builder().setId(usuario).setSubject(userName).setAudience(rol).setIssuedAt(new Date()).setExpiration(fechaExpiracion)
 				.signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
 		
 		return token;
@@ -64,4 +90,37 @@ public class JwtTokenProvider {
 			throw new AppPetsCareExeption(HttpStatus.BAD_REQUEST, "las cadena claims estan vacias");
 		}
 	}
+	
+	//metodo probicional para generar el token con los datos del usuario
+	public String idDeUsuario(String userName) {
+		
+		boolean usu = usuarioRepository.findByCorreoUs(userName).isPresent();
+		boolean clinica = clinicaRepository.findByCorreoCv(userName).isPresent();
+		boolean veterinario = veterinarioRepository.findByCorreo(userName).isPresent();
+		
+		String idUsuario = null;
+		
+		if (usu == true) {
+			UsuarioVo usuario=usuarioRepository.findByNombreUsOrCorreoUs(userName, userName)
+					.orElseThrow(() -> new UsernameNotFoundException("Usuario no Encontrado"));
+			idUsuario = usuario.getDocumentoUs().toString();
+		}
+		else if (clinica == true){
+			ClinicaVo cli=clinicaRepository.findByNombreOrCorreoCv(userName, userName)
+					.orElseThrow(() -> new UsernameNotFoundException("Usuario no Encontrado"));
+			idUsuario = cli.getNit().toString();
+		}
+		else if (veterinario == true) {
+			VeterinarioVo vete=veterinarioRepository.findByNombreOrCorreo(userName, userName)
+					.orElseThrow(() -> new UsernameNotFoundException("Usuario no Encontrado"));
+			idUsuario = vete.getDocumento().toString();
+		}
+		else {
+			idUsuario = "usuarios no encotrados con estas credenciales";
+		}
+		
+		return idUsuario;
+		
+	}
+	
 }
